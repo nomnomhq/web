4205b4490d43111a51ef06bf58311432375b1366
hrshkshri <harshkeshari100@gmail.com>
2025-07-24 02:08:28 +0530

    ui

--- README.md
+++ README.md
@@ -52,4 +52,5 @@ export default tseslint.config({
   },
 });
 ```
+
 # internship-frontend
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
-  return (
-    <div>
-      hello from login
-    </div>
-  )
-}
+  return <div>hello from login</div>;
+};
 
-export default Login
+export default Login;
